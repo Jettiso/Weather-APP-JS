dfb38bcb19d8ae54bc15dfe4e7cd7941b0b3df2b
unknown <ilmsmjettison@gmail.com>
2022-03-24 01:01:08 +0800

    save

--- app.js
+++ app.js
@@ -23,14 +23,17 @@ locationBtn.addEventListener("click", () => {
     }
 });
 
+
+
 function requestApi(city) {
-    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=your_api_key`;
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f9121ac6a26f9b46eec0ca7f776ee29b`;
+    console.log(api)
     fetchData();
-}
+} 
 
 function onSuccess(position) {
     const { latitude, longitude } = position.coords;
-    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=your_api_key`;
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=f9121ac6a26f9b46eec0ca7f776ee29b `;
     fetchData();
 }
 
